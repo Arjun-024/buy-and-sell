4939c87226f4e53995e6f69247758ec3017101d2
Arjun024 <mallikarjuna.gunisetti@gmail.com>
2023-07-10 16:41:02 +0530

    feat: search products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 function Home() {
   const [products, setProducts] = React.useState([]);
+  const [searchProducts, setSearchProducts] = React.useState([]);
 
   React.useEffect(() => {
     fetch(
@@ -15,37 +16,67 @@ function Home() {
         for (let dataItem in data) {
           productList.push({ ...data[dataItem], productId: dataItem });
         }
-
-        console.log(productList);
         setProducts(productList);
       });
   }, []);
 
+  const handleSearch = (e) => {
+    setSearchProducts(
+      products.filter((product) => product.itemTitle.toLowerCase().includes(e.target.value.toLowerCase()))
+    );
+  };
+
+  console.log("search", searchProducts);
+
   return (
-    <div className="product-list-container">
-      {products.length > 0 &&
-        products.map((product) => <ProductCard {...product} />)}
-    </div>
+    <>
+      <div className="product-search-container">
+        <input
+          type="text"
+          placeholder="search products..."
+          onChange={handleSearch}
+        />
+      </div>
+      
+      <div className="product-list-container">
+        {/* {All the products} */}
+        {products.length > 0 &&
+          searchProducts.length == 0 &&
+          products.map((product) => <ProductCard {...product} />)}
+
+        {/* {Seach Product} */}
+        {searchProducts.length > 0 &&
+          searchProducts.map((product) => <ProductCard {...product} />)}
+      </div>
+    </>
   );
 }
 
-function ProductCard({authorContact, authorName, itemDate, itemDesc, itemImg, itemPrice, itemTitle, productId}) {
-  console.log(authorContact)
+function ProductCard({
+  authorContact,
+  authorName,
+  itemDate,
+  itemDesc,
+  itemImg,
+  itemPrice,
+  itemTitle,
+  productId,
+}) {
+  // console.log(authorContact);
   return (
-    <Link to={'/productdetails/' + productId}>
-      <div className='product-card'>
+    <Link to={"/productdetails/" + productId}>
+      <div className="product-card">
         <div className="product-img">
           <img src={itemImg} alt="" />
         </div>
         <div className="product-body">
           <h2>₹ {itemPrice}</h2>
-          <h4>{itemTitle}</h4>
+          <h4>{itemTitle.length > 25 ? itemTitle.slice(0, 25) : itemTitle}</h4>
           <p>{itemDate}</p>
         </div>
       </div>
     </Link>
-  )
+  );
 }
-
 
 export default Home;
